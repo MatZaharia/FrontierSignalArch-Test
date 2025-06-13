f7b2b3ce66372f852d2fbf01d1bfcc5aab5a5954
MatZaharia <guozh29@mail2.sysu.edu.cn>
2025-06-13 19:39:51 +0800

    1

---
+++ mcp/tools/collect.py
@@ -0,0 +1,12 @@
+
+class BaseTool:
+    def __init__(self,name,description,args_schema) -> None:
+        return
+    def run():
+        pass 
+
+class FetchArxivPapersTool(BaseTool):
+    def __init__(self,name,description,args_schema) -> None:
+        return
+    def run():
+        pass 
