e6ded2b9472c16ca8784959503b0d8484b833951
MatZaharia <guozh29@mail2.sysu.edu.cn>
2025-06-14 22:37:00 +0800

    Remove main.py, no longer used

--- main.py
+++
@@ -1,7 +0,0 @@
-
-async def main():
-    async with Client("http://127.0.0.1:8001/sse") as mcp_client:
-        tools = await mcp_client.list_tools()
-        print(f"Available tools: {tools}")
-        result = await mcp_client.call_tool("collect", {"source": 'arXiv', "date": '2025-06-13'})
-        print(f"Result: {result[0].text}")
